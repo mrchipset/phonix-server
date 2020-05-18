2cd10b3b45adc805114054f429019ed01a2c1611
X.F.Zhou <zhouxinfeng@srlab.com.cn>
2020-05-18 17:55:00 +0800

    FEAT: Create project from a asp.net core api template

---
+++ phonix-api/Controllers/PictureItemsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace phonix_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PictureItemsController : ControllerBase
+    {
+
+        private readonly ILogger<PictureItemsController> _logger;
+
+        public PictureItemsController(ILogger<PictureItemsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<PictureItem> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new PictureItem
+            {
+                Date = DateTime.Now.AddDays(index),
+                Owner = rng.Next(),
+                Summary = "HelloString"
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ phonix-api/Models/PictureItem.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace phonix_api
+{
+    public class PictureItem
+    {
+        public DateTime Date { get; set; }
+
+        public int Owner { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
